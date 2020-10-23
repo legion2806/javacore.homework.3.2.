ed64e83996f1efad48d835dc01ccf5ccbdd87b0d
legion2806 <benito2856@gmail.com>
2020-10-23 20:41:31 +0300

    Initial commit Error1

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+
+        Collection<Person> persons = new ArrayList<>();
+
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+
+                    families.get(new Random().nextInt(families.size())),
+
+                    new Random().nextInt(100),
+
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+        Collection<Person> stream = persons
+                .stream()
+                .filter(x -> x > 18)
+                .count();
+
+
+    }
+
+}
